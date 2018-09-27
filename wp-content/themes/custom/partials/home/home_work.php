a2bd7e61bf8ad154c9b2aff31dcaf9bf595ae1bc
Greg Nemes <mail@gregnemes.com>
2018-09-27 07:59:01 -0400

    work on home page

--- wp-content/themes/custom/partials/home/home_work.php
+++ wp-content/themes/custom/partials/home/home_work.php
@@ -1,13 +1,16 @@
-<section class="block section-padded spy-target bg-dark" id="home-work">
+<section class="block section-padded bg-light spy-target" id="home-work">
 	<div class="container-fluid container-fluid-stretch">
 		<div class="row section-header-row">
 			<div class="col-lg-12 col-xl-8">
-				<h3 class="section-header white">
-                    
+				<h3 class="section-header">
+					Featured Artworks
 				</h3>
 			</div>
 		</div>
 		<div class="row section-content-row mb4">
+			<div class="slick-featured-artworks slick">
+
+			</div>
 		</div>	
 	</div>
 </section>
