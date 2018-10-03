24e30c081e7ed0c4be925750ce94949fac96c5aa
Nic Schumann <nicolas_schumann@brown.edu>
2018-10-03 13:55:57 -0400

    [zoho] refactor helpers

--- wp-content/themes/custom/js/module-zoho-connection.js
+++ wp-content/themes/custom/js/module-zoho-connection.js
@@ -20,83 +20,82 @@ const authtoken = '1864b7e6b391f6b94d5cae6a8a9bbd60';
 import { Artwork } from './module-zoho-artwork.js';
 
 
+/**
+ * Given a viewname, optional condition selector, and optional fieldname selector,
+ * make a Zoho API request URI.
+ */
+function makeZohoUri( viewname, condition = false, fieldname = false ) {
+    var uri =  [
+            [zoho_base_uri, 'json', database_name, 'view', viewname ].join('/'),
+            '?authtoken='+authtoken+'&scope=creatorapi&zc_ownername='+ownername,
+            ( condition ) ? '&criteria=(' +condition + ')' : '',
+            ( fieldname ) ? '&' + fieldname : ''
+
+        ].join('');
+
+
+    return uri;
+}
+
+/**
+ * Given a viewname, filtering criterion, and optional selector function, return
+ * a function that can be invoked with a callback to get a set of resources from zoho.
+ *
+ */
+function get_resource( view_name, criterion, fieldname, selector = function( x ) { return x; } ) {
+
+    var uri = makeZohoUri( view_name, criterion, fieldname );
+
+    return function( done ) {
+        $.ajax({
+            crossDomain: true,
+            url: uri,
+            dataType: 'jsonp',
+            type: 'GET',
+            success: function( v ) { done( null, selector( v ) ); },
+            error: done
+        });
+    };
+}
+
+
+/**
+ * Given a string representation of an array '[a,b,c]',
+ * return an array of strings ['a', 'b', 'c'].
+ */
+function unpackStringArray( arr ) {
+
+    var result = arr.split(',');
+
+    result[0] = result[0].substring( 1 );
+
+    result[ result.length - 1 ] = result[ result.length - 1 ].substring( 0, result[ result.length - 1 ].length - 1 );
+
+    result = result.map( function( m, i ) {
+        if ( i > 0 ) { return m.substring( 1 ); }
+        else { return m; }
+    });
+
+    return result;
+
+}
+
+
+/**
+ * ZohoConnection Module.
+ * This module
+ */
 function ZohoConnection() {
     console.log('creating new ZohoConnection instance.');
     if ( !(this instanceof ZohoConnection)) { return new ZohoConnection(); }
     var self = this;
 
-    const options = {
-        method: 'GET',
-        uri: [zoho_base_uri, 'json', database_name, 'view', view_name ].join('/') ,
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Content-Type': 'text/plain'
-        },
-        qs: {
-            authtoken: authtoken,
-            scope: 'creatorapi',
-            zc_ownername: 'the_avenue_concept',
-            raw: true
-        }
-    };
-
-
-    function makeZohoUri( viewname, condition = false, fieldname = false ) {
-        var uri =  [
-                [zoho_base_uri, 'json', database_name, 'view', viewname ].join('/'),
-                '?authtoken='+authtoken+'&scope=creatorapi&zc_ownername='+ownername,
-                ( condition ) ? '&criteria=(' +condition + ')' : '',
-                ( fieldname ) ? '&' + fieldname : ''
-
-            ].join('');
-
-
-        return uri;
-    }
-
-
-    function getMediaCriterion( artwork_media ) {
-
-        var media = artwork_media.split(',');
-
-        media[0] = media[0].substring( 1 );
-
-        media[ media.length - 1 ] = media[ media.length - 1 ].substring( 0, media[ media.length - 1 ].length - 1 );
-
-        media = media.map( function( m, i ) {
-            if ( i > 0 ) { return m.substring( 1 ); }
-            else { return m; }
-        });
-
-        return media;
-
-    }
-
-    function get_resource( view_name, criterion, fieldname, selector = function( x ) { return x; } ) {
-
-        var uri = makeZohoUri( view_name, criterion, fieldname );
-
-        return function( artist_done ) {
-            $.ajax({
-                crossDomain: true,
-                url: uri,
-                dataType: 'jsonp',
-                type: 'GET',
-                success: function( v ) {
-                    artist_done( null, selector( v ) );
-                },
-                error: artist_done
-            });
-        };
-    }
-
 
     self.getArtworks = function( parameters, callback = function() {} ) {
 
-
         $.ajax({
             crossDomain: true,
-            url: [options.uri, '?authtoken='+authtoken+'&scope=creatorapi&zc_ownername='+ownername].join(''),
+            url: makeZohoUri( view_name ),
             dataType: 'jsonp',
             type: 'GET',
             success: function( d ) {
@@ -110,7 +109,7 @@ function ZohoConnection() {
                             //    get_resource( 'All_Locations', 'Location_Name == \"'+ artwork.Location_Name +'\"', function( x ) { return x.Add_Location ),
                                 media: function( media_done ) {
 
-                                    async.parallel( getMediaCriterion( artwork.Add_Media ).map( function( media_name ) {
+                                    async.parallel( unpackStringArray( artwork.Add_Media ).map( function( media_name ) {
                                         return get_resource( 'All_Medias', 'Media_Title == \"' + media_name + '\"', false, function( x ) { return x.Add_Media; });
                                     }), function( err, values ) {
                                         if ( err ) { media_done( err ); }
@@ -137,7 +136,7 @@ function ZohoConnection() {
 
                         callback( null, artworks.map( function( artwork ) {
 
-                            artwork.Medium_field1 = getMediaCriterion( artwork.Medium_field1 );
+                            artwork.Medium_field1 = unpackStringArray( artwork.Medium_field1 );
 
                             return new Artwork( artwork );
 
