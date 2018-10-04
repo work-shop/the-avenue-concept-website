a8ed1b031390527f39cbd43481aebc45f308026a
Greg Nemes <mail@gregnemes.com>
2018-10-04 16:08:41 -0400

    added sidebar buttons

--- wp-content/themes/custom/partials/artworks/sidebar.php
+++ wp-content/themes/custom/partials/artworks/sidebar.php
@@ -1,3 +1,79 @@
 	<div class="artworks-sidebar">
+		<div class="sidebar-row" id="sidebar-view">
+			<h5 class="sidebar-row-heading">
+				View As:
+			</h5>
+			<a href="#" class="sidebar-view-button sidebar-button" data-artworks-view="map">
+				Map
+			</a>
+			<a href="#" class="sidebar-view-button sidebar-button" data-artworks-view="thumbnails">
+				Thumbnails
+			</a>
+			<a href="#" class="sidebar-view-button sidebar-button" data-artworks-view="list">
+				List
+			</a>
+		</div>
+		<div class="sidebar-row" id="sidebar-status">
+			<h5 class="sidebar-row-heading">
+				Status:
+			</h5>
+			<input class="sidebar-status-input" type="checkbox" name="on-view" value="on-view" checked><label class="sidebar-status-label">On View Now</label>
+			<input class="sidebar-status-input" type="checkbox" name="archived" value="archived"><label class="sidebar-status-label">Archived</label>
+		</div>
+		<div class="sidebar-row" id="sidebar-program">
+			<h5 class="sidebar-row-heading">
+				Filter By Program:
+			</h5>
+			<a href="#" class="sidebar-program-button sidebar-button" data-artworks-filter="3-d">
+				3-D
+			</a>
+			<a href="#" class="sidebar-program-button sidebar-button" data-artworks-filter="2-d">
+				2-D
+			</a>
+			<a href="#" class="sidebar-program-button sidebar-button" data-artworks-filter="Wayfinding">
+				Wayfinding
+			</a>
+		</div>
+		<div class="sidebar-row" id="sidebar-year">
+			<h5 class="sidebar-row-heading">
+				Filter By Year:
+			</h5>
+			<div class="row">
+				<div class="col-lg-6">
+					<h4 class="sidebar-row-label">From:</h4>
+					<select>
+						<option value="2012">2012</option>
+						<option value="2013">2013</option>
+						<option value="2014">2014</option>
+						<option value="2015">2015</option>
+						<option value="2016">2016</option>
+						<option value="2017">2017</option>
+						<option value="2018">2018</option>
+					</select>
+				</div>
+				<div class="col-lg-6">
+					<h4 class="sidebar-row-label">To:</h4>
+					<select>
+						<option value="2012">2012</option>
+						<option value="2013">2013</option>
+						<option value="2014">2014</option>
+						<option value="2015">2015</option>
+						<option value="2016">2016</option>
+						<option value="2017">2017</option>
+						<option value="2018">2018</option>
+					</select>
+				</div>
+			</div>
+		</div>
+		<div class="sidebar-row" id="sidebar-location">
+			<h5 class="sidebar-row-heading">
+				Filter By Location:
+			</h5>
+			<select>
+				<option value="downtown">Downtown</option>
+				<option value="west-side">West Side</option>
+				<option value="kennedy-plaza">Kennedy Plaza</option>
+			</select>
+		</div>
 	</div>
 </section><!--close #artworks, opened in artworks/main.php-->
