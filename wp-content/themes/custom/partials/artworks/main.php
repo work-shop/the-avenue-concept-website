607dab25bab7f4614cb90a2224636d18242c005d
Nic Schumann <nicolas_schumann@brown.edu>
2018-10-08 10:58:00 -0400

    [artworks] add artworks stage divs

--- wp-content/themes/custom/partials/artworks/main.php
+++ wp-content/themes/custom/partials/artworks/main.php
@@ -1,3 +1,7 @@
 <section class="block artworks-loading" id="artworks">
-	<div class="artworks-main">
+	<div class="artworks-main" id="artworks-map">
+	</div>
+	<div class="artworks-main" id="artworks-thumbnails">
+	</div>
+	<div class="artworks-main" id="artworks-list">
 	</div>
