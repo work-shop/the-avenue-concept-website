d4f982e9e67791ab361a08552377c80a0f5390fd
Greg Nemes <mail@gregnemes.com>
2018-10-10 16:21:54 -0400

    improved artworks page

--- wp-content/themes/custom/partials/artworks/main.php
+++ wp-content/themes/custom/partials/artworks/main.php
@@ -1,19 +1,23 @@
 <section class="block artworks-loading" id="artworks">
-	<div class="artworks-main" id="artworks-map">
-		<div id="artworks-map-map">
+	<div id="artworks-track">
+		<div class="artworks-main" id="artworks-map">
+			<div id="artworks-map-map">
+			</div>
+			<div class="artworks-list" id="artworks-map-list">
+				<?php get_template_part('partials/artworks/list_header'); ?>
+				<div class="artworks-list-list" id="artworks-map-list-list">
+				</div>
+			</div>
 		</div>
-		<div class="artworks-list" id="artworks-map-list">
-			<?php get_template_part('partials/artworks/list_header'); ?>
-			<div class="artworks-list-list" id="artworks-map-list-list">
+		<div class="artworks-main" id="artworks-thumbnails">
+			<div class="row-broken" id="artworks-thumbnails-row">
+			</div>
+		</div>
+		<div class="artworks-main artworks-list" id="artworks-list">
+			<div class="artworks-list-list" id="artworks-list-list">
 			</div>
 		</div>
 	</div>
-	<div class="artworks-main" id="artworks-thumbnails">
-		<div class="row-broken" id="artworks-thumbnails-row">
-		</div>
-	</div>
-	<div class="artworks-main artworks-list" id="artworks-list">
+	<div class="artworks-list-list-header">
 		<?php get_template_part('partials/artworks/list_header'); ?>
-		<div class="artworks-list-list" id="artworks-list-list">
-		</div>
 	</div>
