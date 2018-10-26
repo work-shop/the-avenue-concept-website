5d842a0daf267d2a453b98552125adf2b5887f85
Greg Nemes <mail@gregnemes.com>
2018-10-26 08:07:02 -0400

    completed press page

--- wp-content/themes/custom/partials/press/coverage.php
+++ wp-content/themes/custom/partials/press/coverage.php
@@ -1,23 +1,28 @@
-<?php if( get_field('coverage')): ?>
-<section class="block padded bg-white" id="press-coverage">
-	<div class="container-fluid container-fluid-stretch">
-		<div class="row section-header-row">
-			<div class="col-xl-8">
-				<h3 class="mb2">
-					<?php the_field('coverage_heading'); ?>
-				</h3>
+<?php if( have_rows('coverage_years') ): ?>
+	<section class="block pt3 pb2 bg-white" id="press-coverage">
+		<div class="container-fluid container-fluid-stretch">
+			<div class="row section-header-row">
+				<div class="col-xl-8">
+					<h3 class="mb2">
+						<?php the_field('coverage_heading'); ?>
+					</h3>
+				</div>
 			</div>
-		</div>
-		<div class="row section-content-row mb2">
-			<div class="col">
-				<p class="mb2">
-					<?php the_field('press_kit_description'); ?>
-				</p>
-				<?php $link = get_field('press_kit_link');
-				if( $link ): ?>
-					<a class="button" href="<?php echo $link['url']; ?>" target="<?php echo $link['target']; ?>"><?php echo $link['title']; ?></a>
-				<?php endif; ?>
+			<div class="row">
+				<?php  while ( have_rows('coverage_years') ) : the_row(); ?>
+					<div class="col-md-4 col-lg-3 year mb3">
+					<?php if( get_sub_field('year_title') ): ?>
+						<h4 class="year-title">
+							<?php the_sub_field('year_title'); ?>
+						</h4>
+					<?php endif; ?>
+					<?php if( get_sub_field('year_articles') ): ?>
+						<div class="year-content wysiwyg">
+							<?php the_sub_field('year_articles'); ?>
+						</div>
+					<?php endif; ?>
+					</div>					
+				<?php endwhile; ?>
 			</div>
-		</div>
-	</section>
-<?php endif; ?>
+		</section>
+	<?php endif; ?>
--- wp-content/themes/custom/partials/press/intro.php
+++ wp-content/themes/custom/partials/press/intro.php
@@ -3,25 +3,32 @@
 		<div class="row">
 			<div class="col col-lg-10 offset-lg-2">
 				<ul class="page-nav-list">
-					<li>
-						<a href="#social-media" class="jump">
-							Social Media
-						</a>
-					</li>
-					<li>
-						<a href="#videos" class="jump">
-							Videos
-						</a>
-					</li>
-					<?php if( get_field('press_kit_heading') || get_field('press_kit_description') || get_field('press_kit_link')): ?>
+					<?php if( get_field('coverage_years') ): ?>
 						<li>
-							<a href="#press-kit" class="jump">
-								Press Kit
+							<a href="#press-coverage" class="jump">
+								Coverage
 							</a>
 						</li>
 					<?php endif; ?>
-				</ul>
-			</div>
+					<?php if( get_field('press_kit_heading') || get_field('press_kit_description') || get_field('press_kit_link')): ?>
+					<li>
+						<a href="#press-kit" class="jump">
+							Press Kit
+						</a>
+					</li>
+				<?php endif; ?>
+				<li>
+					<a href="#videos" class="jump">
+						Videos
+					</a>
+				</li>
+				<li>
+					<a href="#social-media" class="jump">
+						Social Media
+					</a>
+				</li>
+			</ul>
 		</div>
 	</div>
+</div>
 </div>
