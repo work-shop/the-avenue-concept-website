97a17b2a858bbe0c4dc366ca240e77aa66d28e8e
Greg Nemes <mail@gregnemes.com>
2019-07-01 06:30:28 -0400

    minor changes

--- wp-content/themes/custom/partials/donate/donation.php
+++ wp-content/themes/custom/partials/donate/donation.php
@@ -28,7 +28,7 @@ endif;
 		<div class="row">
 			<div class="col-lg-6 col-xl-5 donation-appeal mb2">
 				<?php if (is_page(1254)){ ?>
-					<div class="wysiwyg membership-description">
+					<div class="wysiwyg membership-description mb4">
 						<?php the_field('membership_description'); ?>
 					</div>
 				<?php } else{ ?>
